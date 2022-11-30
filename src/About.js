a2e7ea357e968918d2efb82d319cb363c9049ee0
Thecoder0012 <mardineliassi57@gmail.com>
2022-11-30 01:43:40 +0100

    Routing

---
+++ src/About.js
@@ -0,0 +1,10 @@
+
+export const About = () => {
+    return (
+        <div>
+            <h1>
+                This is About
+            </h1>
+        </div>
+    )
+}
--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import {Routes,Route,Link} from "react-router-dom"
+import {Home} from './Home'
+import {About} from './About'
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <>      
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+
+            <li>
+              <Link to="about">About</Link>
+            </li>
+          </ul>
+
+          <Routes>
+            <Route path="/" element={<Home /> }/>
+            <Route path="/about" element={<About /> }/>
+          </Routes>
+
+
+    </>
   );
 }
 
