56d38024341d3ef93970b462b24d89e5c2d9b993
Thecoder0012 <mardineliassi57@gmail.com>
2022-11-30 23:53:25 +0100

    Styling changes, Navbar component and products.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import './App.css';
 import {Routes,Route,Link} from "react-router-dom"
 import {Home} from './components/Home'
 import {About} from './components/About'
+import { Navbar } from './components/Navbar';
+import { Products } from './components/Products';
 
 function App() {
   return (
     <>      
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-
-            <li>
-              <Link to="about">About</Link>
-            </li>
-          </ul>
-
+        
+          <Navbar />
 
           <Routes>
             <Route path="/" element={<Home /> }/>
+            <Route path="/products" element={<Products /> }/>
             <Route path="/about" element={<About /> }/>
           </Routes>
       
