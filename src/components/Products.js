1f902e501bd1001829078959f3e26424d4c4950e
Unknown1321 <arenstorffenterprise@gmail.com>
2022-12-05 12:50:19 +0100

    Products component added with css

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,8 +1,42 @@
+import React from 'react'
+import image from "../images/frontpage.jpeg"
+import image1 from "../images/background.jpeg"
+import image2 from "../images/frontpage.jpeg"
+import "../css/Products.css";
+
 
 export const Products = () => {
+    const images = [
+        {
+          id: 1,
+          image: image
+        },
+        {
+          id: 2,
+          image: image1
+        },
+        {
+          id: 3,
+          image: image2
+        }
+      ];
+
     return (
-        <div>
-            <h1>Products page</h1>
-        </div>
-    )
+
+      <><h1>Herunder kan du se en oversigt over alle vores produkter</h1>
+      
+      <div className="img-wrapper">
+        <div className="together"><div><img src={image} /> <div className="description">I am a description</div></div></div>
+        <div className="together"><div><img src={image} /> <div className="description">I am a description</div></div></div>
+        <div className="together"><div><img src={image} /> <div className="description">I am a description</div></div></div>
+        
+        <div className="together"><div><img src={image} /> <div className="description">I am a description</div></div></div>
+        <div className="together"><div><img src={image} /> <div className="description">I am a description</div></div></div>
+        <div className="together"><div><img src={image} /> <div className="description">I am a description</div></div></div>
+      </div></>
+    );
 }
+
+export default Products
+
+
