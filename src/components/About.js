a17fa704d5f353a93ab241272dd9dae292597e29
radwan <radw0001@stud.dk>
2022-12-06 13:17:06 +0100

    about us new

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "../css/About.css";
-import image from "../images/mark.jpg"
+import mark from "../images/mark.jpg"
 
 
 
@@ -8,28 +8,33 @@ export const About = () => {
 
     return (
         <div>
-            <div className="about-section">
-  <h1>Hvem er Butchery og hvad står vi ind for</h1>
+            <div className="about-section"> 
+  <h1> Hvem er vi?</h1>
+  <img className="mark" src={mark} alt="About Image" />
+
 
   
-    <h2>Butchery Nørrebro</h2>
-    <p>Butchery Nørrebro har eksisteret siden 1950 og er blabla</p>
+    <h2>Vores historie</h2>
+    <p>Hos slagteren har vi siden 1955 leveret kød til mange glade kunder!
+        Fra Nørrebrogade 22 hvor Hr. Hansen åbnede den første butik, udaf i dag over 12 butikker
+        i hele København og Nordsjælland! Vi bestræber os altid efter at yde det bedste service
+        til alle vores kunder.
+    </p>
     
   
     <h3>Økologisk kød</h3>
-    <p>Hos Butchery Nørrebro vælger vi ku fra det bedste! Vi har derfor i over 10år haft en fast aftale
-       med økologigården Nordsjælland blablabla</p>
+    <p>Hos Slagteren vælger vi med omhu! Derfor har vi i over 10år haft en fast aftale
+       med Økologigården Nordsjælland, for at levere frisk kød af dyr, der er blevet behandlet ordenligt!</p>
   
        <h4>Kontakt os i dag!</h4>
        <p>Tøv ikke med at kontakte os!</p>
-       <p>Du kan skrive til os på vores mail: jane@example.com og vi vil bestræbe os efter at svare så hurtigt som muligt!</p>
+       <p>Du kan skrive til os på vores mail: slagteren@hotmail.com og vi vil bestræbe os efter at svare så hurtigt som muligt!</p>
   
-            <p>Du kan også fange os på tlf. nr.: 27282930 alle hverdage fra 09-15 og weekender fra 10-13</p>
+            <p>Du kan også fange os på tlf. nr.: 123456789 alle hverdage fra 09-15 og weekender fra 10-13.</p>
             <p>Vi glæder os til at høre fra dig!</p>
-            <p>Bedste hilsner: Dit slagter team, Butchery Nørrebro</p>
+            <p>Bedste hilsner, dit slagter team hos Slagteren.</p>
   
   </div>
-            
                 </div>
                 
 
