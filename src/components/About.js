a31d4b7e17ca12a07b480d87788b47c174c07496
radwan <radw0001@stud.dk>
2022-12-06 12:12:19 +0100

    About us

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,44 @@
+import React from "react"
+import "../css/About.css";
+import image from "../images/mark.jpg"
+
+
 
 export const About = () => {
+
     return (
         <div>
-            <h1>
-                This is About
-            </h1>
-        </div>
-    )
+            <div className="about-section">
+  <h1>Hvem er Butchery og hvad står vi ind for</h1>
+
+  
+    <h2>Butchery Nørrebro</h2>
+    <p>Butchery Nørrebro har eksisteret siden 1950 og er blabla</p>
+    
+  
+    <h3>Økologisk kød</h3>
+    <p>Hos Butchery Nørrebro vælger vi ku fra det bedste! Vi har derfor i over 10år haft en fast aftale
+       med økologigården Nordsjælland blablabla</p>
+  
+       <h4>Kontakt os i dag!</h4>
+       <p>Tøv ikke med at kontakte os!</p>
+       <p>Du kan skrive til os på vores mail: jane@example.com og vi vil bestræbe os efter at svare så hurtigt som muligt!</p>
+  
+            <p>Du kan også fange os på tlf. nr.: 27282930 alle hverdage fra 09-15 og weekender fra 10-13</p>
+            <p>Vi glæder os til at høre fra dig!</p>
+            <p>Bedste hilsner: Dit slagter team, Butchery Nørrebro</p>
+  
+  </div>
+            
+                </div>
+                
+
+
+        
+    );
 }
+export default About;
+
+
+
+
